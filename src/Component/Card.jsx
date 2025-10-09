e77d83639616d9456fe85685e5afb5855b9b8494
Alamin <alaminetc9@gmail.com>
2025-10-09 12:31:22 +0600

    appsDetails page functionality added done

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import iconD from '../assets/icon-downloads.png'
 import iconR from '../assets/icon-ratings.png'
+import { Link } from 'react-router';
 
 const Card = ({ app }) => {
     const { title, image, downloads, ratingAvg, id } = app
     return (
         <div>
-            <div className='card bg-[#FFFFFF]  rounded-lg p-3 shadow-lg'>
-                <img className="w-[285px] h-[285px] rounded-lg mx-auto inline-block" src={image} alt="" />
-                <h2 className='font-bold text-xl my-3'>{title}</h2>
-                <div className='flex justify-between'>
-                    <div className='bg-[#F1F5E8] flex gap-1 py-1 px-3  items-center rounded-sm'>
-                        <img className="w-4 " src={iconD} alt="" />
-                        <p className='text-[#00D390] font-semibold'>{downloads}</p>
-                    </div>
-                    <div className='bg-[#FFF0E1] py-1 px-3 rounded-sm flex gap-2 items-center'>
-                        <img className="w-4" src={iconR} alt="" />
-                        <p className='text-[#FF8811] font-semibold '>{ratingAvg}</p>
+            <Link to={`/apps/${id}`}>
+                <div className='card bg-[#FFFFFF]  rounded-lg p-3 shadow-lg'>
+                    <img className="w-[285px] h-[285px] rounded-lg mx-auto inline-block" src={image} alt="" />
+                    <h2 className='font-bold text-xl my-3'>{title}</h2>
+                    <div className='flex justify-between'>
+                        <div className='bg-[#F1F5E8] flex gap-1 py-1 px-3  items-center rounded-sm'>
+                            <img className="w-4 " src={iconD} alt="" />
+                            <p className='text-[#00D390] font-semibold'>{downloads}</p>
+                        </div>
+                        <div className='bg-[#FFF0E1] py-1 px-3 rounded-sm flex gap-2 items-center'>
+                            <img className="w-4" src={iconR} alt="" />
+                            <p className='text-[#FF8811] font-semibold '>{ratingAvg}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+
+            </Link>
+        </div >
     );
 };
 
--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import iconD from '../assets/icon-downloads.png'
 import iconR from '../assets/icon-ratings.png'
 import iconRe from '../assets/icon-review.png'
+import { useParams } from 'react-router';
+import useApps from '../Hooks/useApps';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const AppDetails = () => {
+
+    const { id } = useParams()
+    const { apps, loading } = useApps()
+
+    if (loading) return <p>Loading.......</p>
+
+    const app = apps.find(a => a.id === Number(id))
+
+
+
+    const { title, image, downloads, ratingAvg, reviews, size, description, ratings } = app
+
+
+
     return (
         <div className='container mx-auto my-12 max-sm:px-5'>
             <div className=''>
                 <div className='flex gap-6 items-center border-b-2 border-gray-300 pb-10 max-sm:flex-col '>
-                    <img className="w-[350px] h-[350px] rounded-lg" src="" alt="" />
+                    <img className="w-[350px] h-[350px] rounded-lg" src={image} alt="" />
                     <div>
                         <div className='border-b-2 border-gray-300 pb-2 mb-3'>
-                            <h2 className='font-bold text-3xl'>Forest: Focus for Productivity</h2>
+                            <h2 className='font-bold text-3xl'>
+                                {title}
+                            </h2>
 
                             <p className='text-[#627382] '>Developed by <span className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] inline-block text-transparent bg-clip-text py-2'>productive.io</span> </p>
                         </div>
@@ -20,27 +40,42 @@ const AppDetails = () => {
                             <div className='space-y-2'>
                                 <img className="w-6" src={iconD} alt="" />
                                 <p className='text-sm'>Downloads</p>
-                                <p className='text-2xl font-bold'>9M</p>
+                                <p className='text-2xl font-bold'>{downloads}</p>
                             </div>
                             <div className='space-y-2'>
                                 <img className="w-6" src={iconR} alt="" />
                                 <p className='text-sm'>Average Ratings</p>
-                                <p className='text-2xl font-bold '>4.9</p>
+                                <p className='text-2xl font-bold '>{ratingAvg}</p>
                             </div>
                             <div className='space-y-2'>
                                 <img className="w-6" src={iconRe} alt="" />
                                 <p className='text-sm'>Total Reviews</p>
-                                <h1 className='font-bold text-2xl '>64K</h1>
+                                <h1 className='font-bold text-2xl '>{reviews}</h1>
                             </div>
 
 
                         </div>
                         <div>
-                            <button className="btn btn-success text-white">Install Now (291 MB)</button>
+                            <button className="btn btn-success text-white">Install Now ({size}MB)</button>
                         </div>
                     </div>
                 </div>
+                <div className='Chart my-6'>
+                    <h1 className='text-2xl font-bold mb-3'>Rating</h1>
 
+                    <ResponsiveContainer width="100%" height={300} >
+                        <BarChart data={ratings} layout="vertical">
+                            <XAxis type="number" stroke="#ff7400" />
+                            <YAxis type="category" dataKey="name" stroke="#ff7400" />
+                            <Tooltip />
+                            <Bar dataKey="count" fill="#191c40" barSize={30}></Bar>
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+                <div className=''>
+                    <h1 className='text-2xl font-bold'>Description</h1>
+                    <p>{description}</p>
+                </div>
             </div>
         </div >
     );
